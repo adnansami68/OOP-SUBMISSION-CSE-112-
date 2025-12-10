28adf965f0a7e184d9bdaf16f3e06a9380fd2dd4
MD.ADNAN SAMI <a.m.adnansami143@gmail.com>
2025-12-10 13:17:33 +0600

    Add C++ program to read and display an integer

---
+++ Chapter-01/Example /1.3/3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int num;
+
+    cout << "Enter an Integer Number: ";
+    cin >> num;
+    cout << "Here is your Number: " << num << "\n";
+
+    return 0 ;
+}
