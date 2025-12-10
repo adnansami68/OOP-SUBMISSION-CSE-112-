e6a51d88046b9111f9763271caf967c6dea3b3d4
MD.ADNAN SAMI <a.m.adnansami143@gmail.com>
2025-12-10 13:24:03 +0600

    Add 4.cpp with a simple Boolean check

---
+++ Chapter-01/Example /1.6/4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    bool outcome;
+
+    outcome=false;
+
+    if(outcome)
+
+        cout<<"The outcome is true"<<endl;
+    else
+        cout<<"The outcome is false"<<endl;
+
+        return 0;
+
+
+}
