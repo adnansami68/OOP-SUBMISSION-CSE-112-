80fbe67075ffd2f61470b5e9c6507720ef3895e1
MD.ADNAN SAMI <a.m.adnansami143@gmail.com>
2025-12-11 21:09:57 +0600

    Create 4.cpp

---
+++ Chapter-05/Example/5.5/4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int f(int a) {
+    return a * a;
+}
+
+int f(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    cout << f(10, 2);
+    cout << f(10);
+
+    return 0;
+}
+
